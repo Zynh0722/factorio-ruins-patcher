469b5eb39c8b043736a24678d1fb5b9304d9fd20
Zynh0722 <Zynh0722@gmail.com>
2023-07-04 20:30:18 -0700

    Switch to phf maps

--- build.rs
+++ build.rs
@@ -14,10 +14,17 @@ fn main() {
 
     let patches_dir = fs::read_dir("patches").unwrap();
 
+    writeln!(patches_file, "use phf::phf_map;").unwrap();
+    writeln!(
+        patches_file,
+        "pub const PATCHES: phf::Map<&str, &str> = phf_map! {{"
+    )
+    .unwrap();
+
     for file in patches_dir {
         let file_name = &file.unwrap().file_name();
         let variable_name = file_name.to_string_lossy();
-        let variable_name = variable_name.split(".").next().unwrap().to_uppercase();
+        let variable_name = variable_name.split(".").next().unwrap();
         let file_path = Path::new("patches").join(&file_name);
 
         let file_contents: Value =
@@ -25,10 +32,12 @@ fn main() {
 
         writeln!(
             patches_file,
-            "pub const {variable_name}: &str = r#\"{file_contents}\"#;"
+            "\t\"{variable_name}\" => r#\"{file_contents}\"#,"
         )
         .unwrap();
     }
+
+    writeln!(patches_file, "}};").unwrap();
 
     println!("cargo:rerun-if-changed=build.rs");
     println!("cargo:rerun-if-changed=patches");
