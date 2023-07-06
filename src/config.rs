ed07b990ccbc30e947852ec37b7c8e0fb39e83aa
Zynh0722 <Zynh0722@gmail.com>
2023-07-06 06:50:00 -0700

    convert to serde_json::to_writer_pretty

--- src/config.rs
+++ src/config.rs
@@ -1,7 +1,5 @@
 use std::{io, path::PathBuf};
 
-use std::io::Write;
-
 #[allow(unused)]
 pub fn config_exists() -> bool {
     get_config_path().is_dir()
@@ -31,13 +29,14 @@ pub fn generate_config_dir() -> io::Result<()> {
         let file_path = patches_path.join(key.to_string() + ".json");
         let parsed_json: serde_json::Value = serde_json::from_str(value).unwrap();
 
-        let mut file_handle = std::fs::OpenOptions::new()
+        let file_handle = std::fs::OpenOptions::new()
             .create(true)
             .write(true)
             .truncate(true)
             .open(&file_path)?;
 
-        write!(file_handle, "{parsed_json:#}")?;
+        // Might be worth considering using a bufwriter here
+        serde_json::to_writer_pretty(file_handle, &parsed_json)?;
     }
 
     Ok(())
